27eb7340c1b0fdf5c71840c854b30e7b8df6a165
dine03 <nadine.s5@schule.bremen.de>
2024-02-19 12:13:30 +0100

    LEARNIX-42 <Anmeldebutton> Style geändert

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import {authGuard} from "./Services/auth.guard";
 import {AppComponent} from "./app.component";
+import {LoginComponent} from "./login/login.component";
 
 export const routes: Routes = [
+  { path:'', component: LoginComponent},
   { path:'test', component: AppComponent, canActivate: [authGuard]},
 ];
