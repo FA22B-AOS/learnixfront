6d2e0d0b448e480ca43e93bd7b64a56cf132cae1
Alex <Alex>
2024-05-30 14:31:46 +0200

    LEARNIX-97 Added join request to cards

--- src/app/workspacecard/workspacecard.component.ts
+++ src/app/workspacecard/workspacecard.component.ts
@@ -1,10 +1,11 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {NgIf} from "@angular/common";
 import {ProgressChartComponent} from "../Components/progress-chart/progress-chart.component";
 import {HttpService} from "../Services/http.service";
 import {KeycloakService} from "keycloak-angular";
 import {Workspace} from "../Models/Workspace";
 import {RouterLink} from "@angular/router";
+import {WorkspaceService} from "../Services/workspace.service";
 
 @Component({
   selector: 'app-workspacecard',
@@ -20,15 +21,28 @@ import {RouterLink} from "@angular/router";
 export class WorkspacecardComponent {
   @Input() workspace!: Workspace;
   @Input() member = true;
+  @Output() requestResult: EventEmitter<string> = new EventEmitter<string>();
 
-  constructor(private httpService: HttpService, private keycloak: KeycloakService) {
+
+  constructor(private httpService: HttpService, private keycloak: KeycloakService, private workspaceService: WorkspaceService) {
   }
 
   ngOnInit(): void {
   }
 
   protected requestAccess() {
-
+    const userGUID: string | undefined = this.keycloak.getKeycloakInstance().subject;
+    if (userGUID != undefined && this.workspace)
+    {
+      this.httpService.requestAccessToWorkspace(this.workspace, userGUID).then(() => {
+        console.log('Request submitted successfully');
+        this.requestResult.emit('success');
+      })
+        .catch((error) => {
+          console.error('Error submitting request', error);
+          this.requestResult.emit('error: ' + error.message);
+        });
+    }
   }
 
 }
